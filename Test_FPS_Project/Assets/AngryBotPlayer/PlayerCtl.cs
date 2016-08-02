5cae8e7d3e979375998fb66376168f5f4693e689
YangWoomin <cicyowoo@naver.com>
2016-08-02 23:02:35 +0900

    2016-8-2(2)

--- Test_FPS_Project/Assets/AngryBotPlayer/PlayerCtl.cs
+++ Test_FPS_Project/Assets/AngryBotPlayer/PlayerCtl.cs
@@ -13,20 +13,13 @@ public class Anim
 
 public class PlayerCtl : MonoBehaviour {
     private Transform tr;
+    private Rigidbody rb;
     private float moveSpeed = 5.0f;
-    private float rotSpeed = 1.2f;
     private Vector3 moveDir;
     public Animation _animation;
     public Anim anim;
 
-    private float beforePosition;
-
-
-    private float dist;
-    private bool dragging = false;
-    private Vector3 offset;
-    private Transform toDrag;
-
+    private PhotonView pv = null;
 
     // Use this for initialization
     void Start () {
@@ -34,71 +27,28 @@ public class PlayerCtl : MonoBehaviour {
         _animation = GetComponentInChildren<Animation>();
         _animation.clip = anim.idle;
         _animation.Play();
+
+        pv = GetComponent<PhotonView>();
+        rb = GetComponent<Rigidbody>();
+        if(pv.isMine)
+        {
+            rb.centerOfMass = new Vector3(0.0f, -0.5f, 0.0f);
+        }
+        else
+        {
+            rb.isKinematic = true;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //Touch touch = Input.GetTouch(0);
-        //if (touch.phase == TouchPhase.Began)
-        //{
-        //    beforePosition = touch.position.x;
-        //}
-        //else
-        //{
-        //    tr.Rotate(Time.deltaTime * 3.0f * new Vector3(0, beforePosition - touch.position.x, 0));
-        //    beforePosition = touch.position.x;
-        //}
+        if(!pv.isMine)
+        {
+            return;
+        }
 
         float accelX = Input.acceleration.x;
         float accelY = -Input.acceleration.y;
-
-
-        /*
-        // ---------------------------------------------------------------------
-        Vector3 v3;
-
-        if (Input.touchCount != 1)
-        {
-            dragging = false;
-        }
-        else
-        {
-            Touch touch = Input.touches[0];
-            Vector3 pos = touch.position;
-
-            // touch start
-            if (touch.phase == TouchPhase.Began)
-            {
-                RaycastHit hit;
-                Ray ray = Camera.main.ScreenPointToRay(pos);
-
-                if (Physics.Raycast(ray, out hit) && (hit.collider.tag == "Player"))
-                {
-                    toDrag = hit.transform;
-                    dist = hit.transform.position.z - Camera.main.transform.position.z;
-                    v3 = new Vector3(pos.x, pos.y, pos.z);
-                    v3 = Camera.main.ScreenToWorldPoint(v3);
-                    offset = toDrag.position - v3;
-                    dragging = true;
-                }
-            }
-
-            // drag
-            if (dragging && (touch.phase == TouchPhase.Moved))
-            {
-                v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
-                v3 = Camera.main.ScreenToWorldPoint(v3);
-                toDrag.position = v3 + offset;
-            }
-
-            // touch termination
-            if (dragging && (touch.phase == TouchPhase.Ended) || (touch.phase == TouchPhase.Canceled))
-            {
-                dragging = false;
-            }
-        }
-        // ----------------------------------------------------------------------------
-        */
 
         // character movement for left and right
         if(accelX < -0.8f) {
---
+++ Test_FPS_Project/Assets/PhotonInit.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhotonInit : MonoBehaviour {
+    public string version = "v1.0";
+
+    void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings(version);
+    }
+
+    void OnJoinedLobby()
+    {
+        Debug.Log("Entered Lobby");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("No rooms");
+        RoomOptions ro = new RoomOptions();
+        ro.MaxPlayers = 2;
+        PhotonNetwork.CreateRoom(null, ro, TypedLobby.Default);
+    }
+
+    void OnJoinedRoom()
+    {
+        Debug.Log("Enter Room");
+        CreatePlayer();
+    }
+
+    void CreatePlayer()
+    {
+        PhotonNetwork.Instantiate("Player", new Vector3(-48.99288f, 15.75f, -15.52526f), Quaternion.identity, 0);
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+}
